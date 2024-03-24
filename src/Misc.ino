f825addf413b209140126745b1f4458193ed4ea2
syed-dce <syed7771@yahoo.com>
2024-03-24 23:44:32 +0530

    memory related changes. Inproved performance

--- src/Misc.ino
+++ src/Misc.ino
@@ -2311,7 +2311,8 @@ void checkRAM(byte id)
   \*********************************************************************************************/
 void tone(uint8_t _pin, unsigned int frequency, unsigned long duration) {
   analogWriteFreq(frequency);
-  analogWrite(_pin,100);
+  //NOTE: analogwrite reserves IRAM and uninitalized ram.
+  analogWrite(_pin,100,132);
   delay(duration);
   analogWrite(_pin,0);
 }
@@ -2526,5 +2527,29 @@ void ArduinoOTAInit()
 
 }
 
+String getBearing(int degrees)
+{
+  const char* bearing[] = {
+    PSTR("N"),
+    PSTR("NNE"),
+    PSTR("NE"),
+    PSTR("ENE"),
+    PSTR("E"),
+    PSTR("ESE"),
+    PSTR("SE"),
+    PSTR("SSE"),
+    PSTR("S"),
+    PSTR("SSW"),
+    PSTR("SW"),
+    PSTR("WSW"),
+    PSTR("W"),
+    PSTR("WNW"),
+    PSTR("NW"),
+    PSTR("NNW")
+  };
+
+    return(bearing[int(degrees/22.5)]);
+
+}
 
 #endif
